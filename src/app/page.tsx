693fb37b3b6984fb9e1e57738666cdc4bd592f96
nin0dev <ninberber@gmail.com>
2024-10-14 12:47:40 +0200

    feat: hubspot chatbot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,6 +71,15 @@ I am a Web enthusiast, specializing in development. Check out my projects and co
       <meta name="apple-mobile-web-app-title" content="Portfolio" />
       <meta name="apple-mobile-web-app-status-bar-style" content="#ffffff" />
       <meta name="theme-color" content="#ffffff" />
+      {/* Start of HubSpot Embed Code */}
+      <script
+        type="text/javascript"
+        id="hs-script-loader"
+        async
+        defer
+        src="//js-eu1.hs-scripts.com/145595772.js"
+      ></script>
+      {/* End of HubSpot Embed Code */}
       <main>
         <div className="mb-20">
           <h2 className="text-3xl">Hello 👋</h2>
