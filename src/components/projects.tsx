1cd087ccbbfa78ec2bfa687cf4028729bead995e
Nino Berber <95708793+nin0dev@users.noreply.github.com>
2025-04-21 14:50:47 +0200

    make project card elements clickable

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const projects = [
   {
@@ -19,37 +20,39 @@ export default function Projects() {
         {projects.map((project, index) => (
           <div className="h-auto" key={index}>
             <div className="p-5 relative shadow-lg rounded overflow-hidden hover:scale-105 transition-transform h-full">
-              <Image
-                src={project.image}
-                priority
-                width={500}
-                height={50}
-                alt={project.altText || "Project thumbnail"}
-                className="mb-5 rounded h-[200px] object-cover"
-              />
-              <h4 className="text-xl mb-2">{project.title}</h4>
-              <p className="text-xs overflow-ellipsis overflow-hidden line-clamp-3">
+              <Link href={project.link} target="_blank">
+                <Image
+                  src={project.image}
+                  priority
+                  width={500}
+                  height={50}
+                  alt={project.altText || "Project thumbnail"}
+                  className="mb-5 rounded h-[200px] object-cover"
+                />
+              </Link>
+              <Link href={project.link} target="_blank"><h4 className="text-xl mb-2">{project.title}</h4></Link>
+              <Link href={project.link} target="_blank"><p className="text-xs overflow-ellipsis overflow-hidden line-clamp-3">
               {project.description}
-              </p>
+              </p></Link>
               <div className="pt-5">
-              {project.github ?
-                <a
-                  href={project.github}
-                  target="_blank"
-                  className="text-sm mb-2 whitespace-nowrap hover:underline"
-                >
-                  Check on Github
-                </a>
-              : null}
-              {project.link ?
-                <a
-                  href={project.link}
-                  target="_blank"
-                  className="text-sm ml-5 whitespace-nowrap hover:underline"
-                >
-                  Check project
-                </a>
-              : null}
+                {project.github ?
+                  <a
+                    href={project.github}
+                    target="_blank"
+                    className="text-sm mb-2 whitespace-nowrap hover:underline"
+                  >
+                    Check on Github
+                  </a>
+                : null}
+                {project.link ?
+                  <a
+                    href={project.link}
+                    target="_blank"
+                    className="text-sm ml-5 whitespace-nowrap hover:underline"
+                  >
+                    Check project
+                  </a>
+                : null}
               </div>
             </div>
           </div>
