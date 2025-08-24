cbe0d848390c284248c6abad0b3eee729ae26e9b
Nino Berber <95708793+nin0dev@users.noreply.github.com>
2025-08-24 17:11:13 +0200

    change variable name reference

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,12 +1,12 @@
 import { createClient } from 'contentful';
 
-const { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN } = process.env;
+const { NEXT_PUBLIC_SPACE_ID, CONTENTFUL_ACCESS_TOKEN } = process.env;
 
-if (!CONTENTFUL_SPACE_ID || !CONTENTFUL_ACCESS_TOKEN) {
+if (!NEXT_PUBLIC_SPACE_ID || !CONTENTFUL_ACCESS_TOKEN) {
   throw new Error('Contentful environment variables are not defined!');
 }
 
 export const contentfulClient = createClient({
-  space: CONTENTFUL_SPACE_ID,
+  space: NEXT_PUBLIC_SPACE_ID,
   accessToken: CONTENTFUL_ACCESS_TOKEN,
 });
