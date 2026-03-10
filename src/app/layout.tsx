22e6f33c36a8605f441103d721068594dff14247
nin0dev <ninberber@gmail.com>
2026-03-10 19:44:28 -0400

    remove: chatbot + posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import Header from '@/components/header';
-import Chatbot from '@/components/chatbot';
-import { PostHogProvider } from './providers'
 
 import { Sora } from 'next/font/google';
 
@@ -17,15 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <PostHogProvider>
-        <body
-          className={`mb-20 pt-8 mx-5 lg:px-40 bg-white ${sora.className} text-black`}
-        >
-          {children}
-          <Chatbot />
-          <Header />
-        </body>
-      </PostHogProvider>
+      <body
+        className={`mb-20 pt-8 mx-5 lg:px-40 bg-white ${sora.className} text-black`}
+      >
+        {children}
+        <Header />
+      </body>
     </html>
   );
 }
